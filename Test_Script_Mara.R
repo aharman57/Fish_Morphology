e372ad9a67679b95789823be3e421c9368bddd4a
MaraMc <mara.mchaffie@gmail.com>
2019-03-28 14:50:52 -0400

    scaled variables, added lmer model

--- Test_Script_Mara.R
+++ Test_Script_Mara.R
@@ -16,9 +16,37 @@ Morph <- (read_csv(file="Morph_Data.csv")
                      Jaw="Jaw Gape")
 )
 
-morph_vars <- c("Length","tibia","tarsus","SCT")
-morph_vars_sc <- paste(morph_vars,"s",sep="_")
-dll_data2 <- dll_data
+#scale response variables of interest:
+Morphdata2 <- Morph %>%
+  mutate(Length = scale(Length), Eye = scale(Eye), `Yolk volume` = scale(`Yolk volume`), Fin = scale(Fin), `Body weight` = scale(`Body weight`), Jaw = scale(Jaw), Yolk = scale(Yolk))
+
+#drop variables we're not using (total weight and condition):
+Morphdata2 <- select(Morphdata2, -c("Total weight", "Condition"))
+
+Morph_melt <- (Morphdata2
+             %>% mutate(units=factor(1:n()))
+             %>% gather(trait,value, -c(units, Age, Treatment))
+             %>% drop_na()
+             %>% arrange(units)
+)
+#got this Warning message:
+#attributes are not identical across measure variables;
+#they will be dropped
+#not sure if this is a problem?
+
+#fit linear model:
+t1 <- system.time(
+  lmer1 <- lmer(value ~ trait:(Age*Treatment) - 1 +
+                  (trait-1|units),
+                data=Morph_melt,
+                control=lmerControl(optCtrl=list(ftol_abs=1e-10),
+                                    optimizer="bobyqa",
+                                    check.nobs.vs.nlev="ignore",
+                                    check.nobs.vs.nRE="ignore"))
+)
+
+summary(lmer1)
+
 ### is this relevant/important??? ###
 covariance1 = cov(Morph)
 print(covariance1)
