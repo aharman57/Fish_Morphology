42a62dcac969872a81de1dba6a9a0fabb383f0ed
Adam <adam_harman@live.com>
2019-03-28 15:51:09 -0400

    small changes

--- Adam_R_Script.R
+++ Adam_R_Script.R
@@ -7,9 +7,18 @@ library(dotwhisker)
 library(ggplot2); theme_set(theme_bw())
 
 Morph <- (read_csv(file="Morph_Data_2016-2017.csv"))
-         
+Morph <- select(Morph,-c(20:25))
+
+Morph_clean <- na.omit(Morph)
+
+Morph_clean <- as.numeric(Morph_clean$`Measure Dorsal Fin Anterior Maximum`)
+Morph_clean <- as.numeric(Morph_clean$`Measure Dorsal Fin Minimum`)
+Morph_clean <- as.numeric(Morph_clean$`Measure Dorsal Fin Posterior Maximum`)
+Morph_clean <- as.numeric(Morph_clean$`Measure Yolk Width`)
+Morph_clean <- as.numeric(Morph_clean$`Measure Yolk Height`)
+       
 ### is this relevant/important??? ###
-covariance1 = cov(Morph)
+covariance1 = cov(Morph[,3:17])
 print(covariance1)
 
 ### correlation matrix for all predictor variables ###
