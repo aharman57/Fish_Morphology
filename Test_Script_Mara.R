99527833616d0cf2f9ce19f9f0a474cd2f86bbc5
MaraMc <mara.mchaffie@gmail.com>
2019-03-29 09:53:10 -0400

    added some cleaning code, still figuring out as.numeric issue so ignore that

--- Test_Script_Mara.R
+++ Test_Script_Mara.R
@@ -8,14 +8,33 @@ library(ggplot2); theme_set(theme_bw())
 library(car)
 library(geomorph)
 
-Morph <- (read_csv(file="Morph_Data.csv")
-          %>% rename(Age="Age (dph)",
-                     Eye="Eye diameter",
-                     Fin="Fin indent",
-                     Yolk="Yolk weight",
-                     Jaw="Jaw Gape")
+Morph <- read_csv("Morph_Data_2016-2017.csv")
+Morph_clean <- (Morph
+          %>% select(-c(1:2, 16:17, 20:25)) #got rid of other variables we probably won't use
+          %>% rename(Length = "Length (mm)",
+                     Eye = "Measure Eye Diameter",
+                     Fin_Anterior = "Measure Dorsal Fin Anterior Maximum",
+                     Fin_Min = "Measure Dorsal Fin Minimum",
+                     Fin_Posterior = "Measure Dorsal Fin Posterior Maximum",
+                     Fin_Indent = "Fin indentation ratio",
+                     Yolk_Width ="Measure Yolk Width",
+                     Yolk_Height = "Measure Yolk Height",
+                     Yolk_Vol = "Yolk volume (mm2)",
+                     Jaw = "Jaw gape (um)",
+                     Body_Weight = "body weight",
+                     Yolk_Weight = "yolk weight"
+          )
+          %>% na.omit()
+          %>% as.numeric(Fin_Anterior)
 )
 
+Morph_clean <- (Morph_clean
+                %>% as.numeric("Fin_Anterior")
+)
+for (i in 1:13) {
+  Morph_clean[,i] <- as.numeric(Morph_clean[,i])
+}
+as.numeric(Morph_clean$Body_Weight)
 #covariance matrix of response traits:
 cov(Morph[,3:11])
 cor(Morph[,3:11])
